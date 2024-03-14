a5690fdab53da7bf77e27feac3170c123e79cf75
Christian Nuss <christian.nuss@gmail.com>
2024-03-14 19:58:04 -0400

    log serverless state

--- src/action.ts
+++ src/action.ts
@@ -1,25 +1,42 @@
+import { debug, notice } from '@actions/core';
 import { getInput } from '@actions/core';
+import { warn } from 'console';
+import fs from 'fs';
+import path from 'path';
 
 const { GITHUB_TOKEN } = process.env;
 
 export class Action {
   async run(): Promise<void> {
+    debug('Running!');
+
     const token = getInput('token') || GITHUB_TOKEN;
 
     if (!token) {
       throw new Error('Missing GitHub Token');
     }
-
-    console.log('Running!');
   }
 
   async post(): Promise<void> {
+    debug('Post Running!');
+
     const token = getInput('token') || GITHUB_TOKEN;
 
     if (!token) {
       throw new Error('Missing GitHub Token');
     }
 
-    console.log('Post!');
+    let serverlessState = {};
+
+    try {
+      serverlessState = JSON.parse(
+        fs.readFileSync(path.join('.serverless', 'serverless-state.json'), 'utf8'),
+      );
+    } catch (e) {
+      warn('No serverless state found, skipping post deployment actions.');
+      return;
+    }
+
+    console.log('!!! serverlessState', serverlessState);
   }
 }
